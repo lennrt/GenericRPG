b53b2b7eeb32123b13b6109b5a48f43b104940ec
Daniel Nguyen <duynguyen1494@yahoo.com>
2016-10-01 09:54:05 -0700

    adding monster stats and monster names

--- enums.h
+++ enums.h
@@ -2,3 +2,7 @@
 enum Races {"Elf", "Dwarf", "Human", "Hobbit"};
 enum Classes {"Fighter", "Mage", "Ranger", "Thief"};
 enum Stats {"Mana", "Health", "Intelligence", "Strength", "Agility"};
+enum Monster_Stats{"Name", "HP", "Exp", "Minimum Attack", "Maximum Attack", "Description"};
+enum Monster{"Hedgehog","Dead Tree", "Giant Frog","Roach Egg", "Cockroach", "Skeleton", 
+             "Violinist","Gru Bat","Anthropod","Zombie","Roddent Tail","Poison Hedgehog",
+             "Crazy Squirrle","Horn","Fire Ant"};
