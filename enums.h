1b101a36f35ffa69f3ba5816f9cf471e79806f04
Nhat Nguyen <rockylikespocky@users.noreply.github.com>
2016-10-01 10:13:15 -0700

    Update enums.h

--- enums.h
+++ enums.h
@@ -3,7 +3,7 @@ enum Races {"Elf", "Dwarf", "Human", "Hobbit"};
 enum Classes {"Fighter", "Mage", "Ranger", "Thief"};
 enum Stats {"Mana", "Health", "Intelligence", "Strength", "Agility"};
 enum Monster_Stats{"Name", "HP", "Exp", "Minimum Attack", "Maximum Attack", "Description"};
-enum Monster{"Hedgehog","Dead Tree", "Giant Frog","Roach Egg", "Cockroach", "Skeleton", 
+enum Monsters{"Hedgehog","Dead Tree", "Giant Frog","Roach Egg", "Cockroach", "Skeleton", 
              "Violinist","Gru Bat","Anthropod","Zombie","Roddent Tail","Poison Hedgehog",
              "Crazy Squirrle","Horn","Fire Ant"};
 enum Tile_Types {"Arid", "Forest", "Water"};
