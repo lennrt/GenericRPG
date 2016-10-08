d3ad9a9f05a54e579c8032bdecdde66752a1c5a0
ctrodney <ctrodney@gmail.com>
2016-10-08 10:13:08 -0700

    Update enums.h

--- enums.h
+++ enums.h
@@ -1,7 +1,6 @@
-
 enum Races {Elf, Dwarf, Human, Hobbit};
 enum Classes {Fighter, Mage, Ranger, Thief};
-enum Stats {Mana, Health, Intelligence, Strength, Agility, Attack, Defense};
-enum Monster_Stats {Health, Exp, Intelligence, Strength, Agility, Attack, Defense};
+enum Stats {Mana, Health, Intelligence, Strength, Agility, Attack, Defense, RefreshTime};
+enum Monster_Stats {Health, Exp, Intelligence, Strength, Agility, Attack, Defense, RefreshTime};
 enum Tile_Types {Arid, Forest, Water};
 enum Target_Types {Enemy, Friendly, Both};
