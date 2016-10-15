7d0d6c5b000741a4f62ae0f122ca3817da911a64
DuyNguyen <duynguyen1494@yahoo.com>
2016-10-15 10:48:40 -0700

    add Monster_Stats
    Separate Monster_Stats from Stats because monster.cpp only uses 5/14
    Stats.

--- enums.h
+++ enums.h
@@ -1,5 +1,6 @@
 enum Races {Elf, Dwarf, Human, Hobbit};
 enum Classes {Fighter, Mage, Ranger, Thief};
 enum Stats {Mana, Health, Experience, Intelligence, Strength, Agility, Constitution, Speed, Charisma, Endurance, Attack, Defense, ActionCoolDown, MagicCoolDown};
+enum Monster_Stats {Health, Experience, Attack, Defense, ActionCoolDown};
 enum Tile_Types {Arid, Forest, Water};
 enum Target_Types {Enemy, Friendly, Both};
