115406e832de0b157f2f880d5f5cfe66c413cc60
ctrodney <ctrodney@gmail.com>
2016-10-22 10:27:36 -0700

    Update enums.h

--- enums.h
+++ enums.h
@@ -1,12 +1,6 @@
 
 enum Races {Elf, Dwarf, Human, Hobbit};
 enum Classes {Fighter, Mage, Ranger, Thief};
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-enum Stats {Mana, Health, Experience, Intelligence, Strength, Agility, Constitution, Speed, Charisma, Endurance, Attack, Defense, ActionCoolDown, MagicCoolDown};
-=======
->>>>>>> Stashed changes
 enum Stats {Mana, Health, Intelligence, Strength, Agility, Constitution, Speed, Charisma, Endurance, Attack, Defense, ActionCoolDown, MagicCoolDown};
 enum Tile_Types {Arid, Forest, Water};
 enum Target_Types {Enemy, Friendly, Both};
