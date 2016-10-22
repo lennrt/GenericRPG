9473a8db47f4a0251adfd1ebf42792c0a80aab99
ctrodney <ctrodney@gmail.com>
2016-10-22 09:31:31 -0700

    Create entity.h

---
+++ entity.h
@@ -0,0 +1,32 @@
+#include <string>
+#include "enums.h"
+
+class Entity{
+    public:
+	    
+	std::string Name;
+	
+        Entity();
+        ~Entity();
+	
+	void AlterExp (int Amount);
+	int GetExp();
+	int GetLevel();
+	void AlterGold (int Amount);
+	int GetGold();
+	
+	int GetStat(Stats Stat, bool Max);
+	void AlterStat (Stats Stat, bool Max, int Amount);
+	void SetStatToMax (Stats Stat);
+
+    private:
+	int _Exp;
+	int _Gold;
+	int _Level;
+	int StatTable[2][14];
+	
+	const int Current = 0;
+	const int Maximum = 1;
+	
+	int SetLevel(int tExp);
+};
