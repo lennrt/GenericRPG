cd9c9b152ab6236514209a1a2e0bcb113116e704
ctrodney <ctrodney@gmail.com>
2016-11-05 09:33:27 -0700

    Account Manager
    Handles all account and character creation / deletion / login
    
    Also handles comms with game service

--- accman.cpp
+++ accman.cpp
@@ -4,11 +4,14 @@
 #include <fstream>
 #include <time.h>
 #include <cstdio>
+#include <cstdlib>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -37,23 +40,21 @@ int main(){
 	fstream PWFile;
 	fstream UserFile;
 	fstream CharFile;
-
-	//Redirect Error messages to stdout - This will cause them to appear on the web page
-	dup2(STDOUT_FILENO, STDERR_FILENO);
 	
 	//Set HTTP Header
 	cout << "Content-type: text/plain\n\r\n\r";
-
+	
 	//Process incoming POST data
 	getFormData();
-
+	
 	//Check action based on POST:
+	
 	// This login is insecure because the password is not encrypted in transit from the browser and because the crypt function
 	// is used which isn't secure.  But it's a decent student project concept of including security elements in a product.
 	Op = GetValueFromKey("Op");		
 	string u = GetValueFromKey("u");
 	string p = GetValueFromKey("p");
-
+	
 	//User Login
 	if (Op == "Login"){
 		if (fileExists ("u/" + u)){
@@ -70,18 +71,20 @@ int main(){
 				Response += "&Status=Success";
 				
 				i=0;
-				j=0;
+				j=1;
 				
 				UserFile.open ("u/" + u + "/u", fstream::in);
 				while ( UserFile >> Temp ){
 					if (i == 0) {Response += "&ID" + to_string(j) + "=" + Temp;}
-					if (i == 1) {Response += "&Char" + to_string(j) + "=" + Temp;}
+					if (i == 1) {Response += "&Name" + to_string(j) + "=" + Temp;}
 					if (i == 2) {Response += "&Level" + to_string(j) + "=" + Temp;}
 					if (i == 3) {Response += "&Race" + to_string(j) + "=" + Temp;}
 					if (i == 4) {Response += "&Class" + to_string(j) + "=" + Temp;}
 					i++;
-					j++;
-					if (i > 4) {i = 0;}
+					if (i > 4) {
+						i = 0;
+						j++;
+					}
 				}
 				UserFile.close();
 				cout << Response;
@@ -96,7 +99,6 @@ int main(){
 			cout << Response;
 			exit(0);	
 		}
-		
 	}
 
 	//User Setup
@@ -120,43 +122,43 @@ int main(){
 		Response += "&Status=Success";
 		cout << Response;
 		exit(0);
-		
 	}
 
 	//Character Login
 	if (Op == "Play"){
-		vector <string> Rebuild;
-		string ID = GetValueFromKey("ID");
-		
-		if (fileExists ("u/" + u)){
-			string PassTestIn(crypt(p.c_str(),"AB"));  //This is EXTREMELY insecure.  Use for student project only.
-			string PassTestFile;
-			
-			//Verify password for action
-			PWFile.open ("u/" + u + "/p", fstream::in);
-			PWFile >> PassTestFile;
-			PWFile.close();
-			
-			if (PassTestFile == PassTestIn){
-				Response += EnterGame();
-				cout << Response;
-				exit(0);
-			} else {
-				Response += "&Status=Fail&Reason=Incorrect Password.";
-				cout << Response;
-				exit(0);	
-			}
-		} else {
-			Response += "&Status=Fail&Reason=User does not exist.";
-			cout << Response;
-			exit(0);	
-		}	
-		
+// 		vector <string> Rebuild;
+// 		string ID = GetValueFromKey("ID");
+// 		
+// 		if (fileExists ("u/" + u)){
+// 			string PassTestIn(crypt(p.c_str(),"AB"));  //This is EXTREMELY insecure.  Use for student project only.
+// 			string PassTestFile;
+// 			
+// 			//Verify password for action
+// 			PWFile.open ("u/" + u + "/p", fstream::in);
+// 			PWFile >> PassTestFile;
+// 			PWFile.close();
+// 			
+// 			if (PassTestFile == PassTestIn){
+// 				Response += EnterGame();
+// 				cout << Response;
+// 				exit(0);
+// 			} else {
+// 				Response += "&Status=Fail&Reason=Incorrect Password.";
+// 				cout << Response;
+// 				exit(0);	
+// 			}
+// 		} else {
+// 			Response += "&Status=Fail&Reason=User does not exist.";
+// 			cout << Response;
+// 			exit(0);	
+// 		}	
 	}
 
 	//Character Setup
 	if (Op == "NewCharacter"){
 		vector <string> Rebuild;
+
+
 		string ID = GetValueFromKey("ID");
 		
 		if (fileExists ("u/" + u)){
@@ -169,6 +171,8 @@ int main(){
 			PWFile >> PassTestFile;
 			PWFile.close();
 			
+
+
 			if (PassTestFile == PassTestIn){
 				Response += "&Status=Success";
 				
@@ -178,37 +182,36 @@ int main(){
 				while ( UserFile >> Temp ){
 					Rebuild.push_back(Temp);
 				}
+
+
 				UserFile.close();
 				
 				//Write out list of characters removing the deleted character.
 				UserFile.open ("u/" + u + "/u", fstream::out | fstream::trunc);
 				for (int i = 0; i < Rebuild.size(); i++){
-					if (Rebuild[i] == ID){
-						i += 4;
-					} else {
-						UserFile << Rebuild[i];
-					}
+					UserFile << Rebuild[i] << endl;
 				}
-				UserFile << GetValueFromKey("ID");
-				UserFile << GetValueFromKey("Char");
-				UserFile << GetValueFromKey("Level");
-				UserFile << GetValueFromKey("Race");
-				UserFile << GetValueFromKey("Class");				
+				UserFile << GetValueFromKey("ID") << endl;
+				UserFile << GetValueFromKey("Name") << endl;
+				UserFile << GetValueFromKey("Level") << endl;
+				UserFile << GetValueFromKey("Race") << endl;
+				UserFile << GetValueFromKey("Class") << endl;				
 				UserFile.close();
 				
 				//Create the character information file for the character.
 				CharFile.open ("u/" + u + "/" + ID, fstream::out);
-				CharFile << "Name=" + GetValueFromKey("Char");
-				CharFile << "Race=" + GetValueFromKey("Race");
-				CharFile << "Class=" + GetValueFromKey("Class");
-				CharFile << "Sex=" + GetValueFromKey("Sex");
-				CharFile << "Int=" + GetValueFromKey("Int");
-				CharFile << "Str=" + GetValueFromKey("Str");
-				CharFile << "Agi=" + GetValueFromKey("Agi");
-				CharFile << "Con=" + GetValueFromKey("Con");
-				CharFile << "Spd=" + GetValueFromKey("Spd");
-				CharFile << "Cha=" + GetValueFromKey("Cha");
-				CharFile << "End=" + GetValueFromKey("End");
+				CharFile << "Name=" + GetValueFromKey("Name") << endl;
+				CharFile << "Race=" + GetValueFromKey("Race") << endl;
+				CharFile << "Class=" + GetValueFromKey("Class") << endl;
+				CharFile << "Sex=" + GetValueFromKey("Sex") << endl;
+				CharFile << "Level=" + GetValueFromKey("Level") << endl;				
+				CharFile << "Int=" + GetValueFromKey("Int") << endl;
+				CharFile << "Str=" + GetValueFromKey("Str") << endl;
+				CharFile << "Agi=" + GetValueFromKey("Agi") << endl;
+				CharFile << "Con=" + GetValueFromKey("Con") << endl;
+				CharFile << "Spd=" + GetValueFromKey("Spd") << endl;
+				CharFile << "Cha=" + GetValueFromKey("Cha") << endl;
+				CharFile << "End=" + GetValueFromKey("End") << endl;
 				CharFile.close();
 				
 				cout << Response;
@@ -259,7 +262,7 @@ int main(){
 					if (Rebuild[i] == ID){
 						i += 4;
 					} else {
-						UserFile << Rebuild[i];
+						UserFile << Rebuild[i] << endl;
 					}
 				}
 				UserFile.close();
@@ -325,6 +328,10 @@ string EnterGame(){
 	//can be made to sleep or another process on another CPU can intervene.  It's much better than nothing and is
 	//simple enough for now.
 	
+	//Mailbox format:  Byte 0 = Taken/Free flag; Byte 1 = Message for server flag; Byte 2 = Message for client flag.  
+	//Byte 3 + 4 = size of message.  Byte 5+ = message.
+	//No thread issues since only one server-client per segment and always only read or write not both.
+	
 	for (int i = 1; i < 255; i++){ 
 		
 		tSlot = i * 32768;
@@ -334,23 +341,14 @@ string EnterGame(){
 			//Claim the slot.
 			controlPointer[tSlot] = 0x1;
 			
-			//Register the slot location.  Wait until the byte-lock is off.
-			while (!Done){
-				if (controlPointer[0] == 0x0){
-					controlPointer[0] = 0x1;
-					Slot = i;
-					Done = true;
-					unsigned char Pos = (unsigned char)controlPointer[1];
-					controlPointer[Pos + 2] = (unsigned char)Slot;
-					Pos++;
-					controlPointer[1] = Pos;
-					controlPointer[0] = 0x0;
-					i = 256;					
-				} else {
-					// Sleep for 3 microseconds and then check again.
-					nanosleep (&req, &rem);
-				}
-			}
+			string User = GetValueFromKey("u");
+			
+			//Message for Server Flag
+			controlPointer[tSlot+1] = 0x1;
+			//Size of Message = 1
+			controlPointer[tSlot+4] = (unsigned char)User.size();
+			//Message = ID of noew Box owner
+			sprintf(controlPointer[tSlot+5], User.c_str());
 		}
 		
 	}
@@ -359,9 +357,7 @@ string EnterGame(){
 	//Response will incluse a session ID, confirmed slot number for message exchange, starting position, 
 	//full character stats, map, nearby enemies...
 	
-	//Mailbox format:  Byte 0 = Taken/Free flag; Byte 1 = Message for server flag; Byte 2 = Message for client flag.  
-	//Byte 3 + 4 = size of message.  Byte 5+ = message.
-	//No thread issues since only one server-client per segment and always only read or write not both.
+
 	
 	RetVal += "&Slot=" + to_string(Slot);
 	
@@ -379,21 +375,30 @@ string EnterGame(){
 
 string GetValueFromKey(string FindKey){
 	for (int i = 0; i < FormData.size(); i++){
-		if (FormData[i].Key == FindKey){ return FormData[i].Value; }
+		if (!FormData[i].Key.compare(FindKey)){ return FormData[i].Value; }
 	}	
 	return "";
 }
 
 void getFormData(){
-	int EnvLen = atoi(getenv("CONTENT_LENGTH"));
-	int Start = 0;
+	int EnvLen;	
+	int Start = 1;
 	int Mid = 0;
 	FormInfo NewData;
 	string Env;
-
+	
+	
+	if (getenv("CONTENT_LENGTH") != NULL){
+		EnvLen = atoi(getenv("CONTENT_LENGTH"));
+	 	cin >> Env;
+	} else {
+		string TEMPstr = "?u=test&p=test&Op=New";
+		EnvLen = TEMPstr.size();
+		Env = TEMPstr;
+	}
+	
 	//cin can technically recieve more than CONTENT_LENGTH.
 	//We'll use EnvLen to restrict what we read.
-	cin >> Env;
 
 	if (EnvLen == 0){
 		//Improper header
@@ -407,48 +412,10 @@ void getFormData(){
 		if (Env[i] == '&' || i == EnvLen - 1){
 			if (i == EnvLen - 1) i++;  //Get last character
 			NewData.Key = Env.substr(Start,Mid-Start-1);
-			NewData.Value = HexToString(Env.substr(Mid, i - Mid));
+			NewData.Value = Env.substr(Mid, i - Mid);
 			FormData.push_back(NewData);
 			Start = i + 1;
 		}
 	}
-}
-
-
-string StringToHex(string Data){
-    string ReturnValue;
-    string Hex("0123456789ABCDEF");
-    unsigned char Hex1;
-    unsigned char Hex2;
-    unsigned char Char;
-
-    for (int i = 0; i < Data.length(); i++){
-        Char = Data[i];
-        Hex1 = Hex[((int)Char) / 16];
-        Hex2 = Hex[((int)Char) % 16];
-        ReturnValue += Hex1;
-        ReturnValue += Hex2;
-    }
-    return ReturnValue;
-}
-
-string HexToString(string Data){
-    string ReturnValue = "";
-    char Temp;
-
-    for (int i = 0; i < Data.length(); i+=2){
-        Temp = HexToInt(Data[i])*16 + HexToInt(Data[i+1]);
-        ReturnValue += Temp;
-    }
-    return ReturnValue;
-}
-
-int HexToInt(char Convert){
-    string Hex("0123456789ABCDEF");
-
-    for (int i = 0; i < 16; i++){
-        if (Convert == Hex[i]) return i;
-    }
-    return -1;
 }
 
