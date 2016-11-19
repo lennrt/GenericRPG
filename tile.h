7ba7f0d4d59b3c007fb3964c90555ca32731dc04
Nhat Nguyen <rockylikespocky@Nhats-MacBook-Air.local>
2016-11-19 10:01:24 -0800

    Removed pack & unpack functions.

--- tile.h
+++ tile.h
@@ -103,10 +103,6 @@ public:
     vector<int> getClimateDesc();
     void setClimateDesc(vector<int> customClimateDesc);
     void generateClimateDesc();
-    
-    // Pack & Unpack Functions
-    string pack();
-    void unpack(string packedString);
 };
 
 
