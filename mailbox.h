27bd59aa13505bfd50c84358710d9f20d2a4df4f
ctrodney <ctrodney@gmail.com>
2016-11-21 01:58:42 -0800

    Update mailbox.h
    Mailbox complete (minus some testing)

--- mailbox.h
+++ mailbox.h
@@ -4,32 +4,30 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-struct Box {
-	string User;
-	int BoxNumber;
-};
-
-struct Message {
-	string User;
-	string Message;
-};
-	
-
 class Mailbox{
     public:
-	vector <Box> Boxes;
-	vector <Message> Messages;
-	    
         Mailbox();
         ~Mailbox();
 	
-	void CheckForNewUsers();
-	void CloseUserBox();
-	void CheckMessages();
-	string GetNextMessage();
-	void SendMessageToUser(string Message, string User);
+	void OpenUserBox(int Box);
+	void CloseUserBox(int Box);
+	std::string GetNextMessage();
+	void SendMessageToBox(std::string Message, int Box);
+	void BroadcastMessage(std::string Message);
+	bool SetupSuccessful();
 
     private:
+	std::vector <std::string> Inbox;
+	std::vector <int> OpenBoxes;
+	
+	void CheckMessages();
+	bool LockInbox();
+	void UnlockInbox();
+	std::string GetMessage(int Slot);
+	void SetMessage(int Slot, std::string Message);
+	void ClearSlot (int Slot);	
+	bool CheckSlot(int Box, int Slot);
+	
 	//shared Memory ID and pointer for control segment.
 	int controlID;
 	char* controlPointer;
@@ -37,9 +35,26 @@ class Mailbox{
 	//Randomly generated key value for shared memory: 920004285
 	key_t controlKey = 920004285;
 	
-	//The shared memory contains 1024 segments of 8192 bytes (8KB) in length.
+	//The shared memory contains 256 segments of 32768 bytes (32KB) in length.
 	size_t controlSize = 8388608;
 	
 	//Size of message boxes
 	int boxSize = 32768;
+	int boxCount = 192;
+	
+	//Size of messages
+	int slotSize = 1024;
+	int slotCount = 32;
+	
+	//Size of Inbox
+	int inboxSize = 2097152;
+	int inboxCount = 2048;
+	
+	//Location of thread lock and inbox controls
+	int InboxLock = 0;
+	int BoxClaimedFlag = 0;
+	int MessageSetFlag = 0;
+	int MessageReceivedFlag = 1;
+	int FlagSetSize = 2;
+	
 };
