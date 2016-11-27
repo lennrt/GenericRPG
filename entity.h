348c8239808e99510dc598e7fe94069c9999615f
ctrodney <ctrodney@gmail.com>
2016-11-26 17:28:56 -0800

    Update entity.h

--- entity.h
+++ entity.h
@@ -1,6 +1,9 @@
 #include <string>
 #include "enums.h"
 
+#ifndef ENTITY_H
+#define ENTITY_H
+
 class Entity{
     public:
 	    
@@ -18,15 +21,17 @@ class Entity{
 	int GetStat(Stats Stat, bool Max);
 	void AlterStat (Stats Stat, bool Max, int Amount);
 	void SetStatToMax (Stats Stat);
-	
+
     private:
 	int _Exp;
 	int _Gold;
 	int _Level;
-    int StatTable[2][13] = {0};
+	int StatTable[2][14];
 	
 	const int Current = 0;
 	const int Maximum = 1;
 	
 	int SetLevel(int tExp);
 };
+
+#endif
