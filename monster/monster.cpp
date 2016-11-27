26c6ae07573a5aef8ad3ddac5d7a0614f51c4531
ctrodney <ctrodney@gmail.com>
2016-11-27 00:38:59 -0800

    Update monster.cpp

--- monster/monster.cpp
+++ monster/monster.cpp
@@ -1,40 +1,15 @@
-/********************************************************************************
- File name: monster.cpp
- Written by: Daniel Nguyen
- Written on 08/27/2016
- Purpose: This is a c++ file containing monster class.
- 
- Class: CPSC 362
- *********************************************************************************/
 #include <sstream>
 #include <string>
 #include "enums.h"
 #include "entity.h"
+#include "monster.h"
 
-/*********************************************************************
- This is the monster class. It only uses Health, Experience, Attack,
- Defense, ActionCoolDown.
- *********************************************************************/
-class Enemy : public Entity{
-private:
-    std::string name;
-public:
-    //Constructor
-    Enemy();
-    
-    //Destructor
-    ~Enemy();
-    
-    //Set the level of the monster
-    void setLevel(int amount)
-    {
-        _Level = amount;
-    }
-    
-    //Accessor function for name
-    std::string get_name(){return name;}
-};
+//Set the level of the monster
+void Enemy::setLevel(int amount){
+	_Level = amount;
+}
 
-
-
-
+//Accessor function for name
+std::string Enemy::get_name(){
+	return name;
+}
