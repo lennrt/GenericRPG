a183a87d6a8fef5a09b6572507c232d5b32cf85d
ctrodney <ctrodney@gmail.com>
2016-11-26 17:29:31 -0800

    Update accman.cpp
    Finalized account manager.  This should be the final product.

--- accman.cpp
+++ accman.cpp
@@ -12,8 +12,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <string.h>
 
 using namespace std;
+
+//Variables and constants for handling shared memory and semaphore.
+
+	//shared Memory and semaphore ID and pointer for control segment.
+	int controlID;
+	char* controlPointer;
+	sem_t* semID;
+	std::string semName("Inbox");
+
+	//Randomly generated key value for shared memory: 920004285
+	key_t controlKey = 920004285;
+	
+	//The shared memory contains 256 segments of 32768 bytes (32KB) in length.
+	size_t controlSize = 8388608;
+	
+	//Size of message boxes
+	int boxSize = 32768;
+	int boxCount = 192;
+	
+	//Size of messages
+	int slotSize = 1024;
+	int slotCount = 32;
+	
+	//Size of Inbox
+	int inboxSize = 2097152;
+	int inboxCount = 2048;
+	
+	//Location of thread lock and inbox controls
+	int InboxLock = 0;
+	int BoxClaimedFlag = 0;
+	int MessageSetFlag = 0;
+	int MessageReceivedFlag = 1;
+	int FlagSetSize = 2;
+	int SessionIDLocation = 3;
+	int SessionIDSize = 8;
 
 //Structure for HTTP form data
 struct FormInfo{
@@ -23,16 +62,21 @@ struct FormInfo{
 
 //Vector to hold Key / Value pairs after processing
 vector<FormInfo> FormData;
+string SerialFormData;
 
-void setupMemory();
-size_t getControlSize();
-void getFormData();
-string GetValueFromKey(string FindKey);
 bool fileExists(const std::string& file);
-string StringToHex(string Data);
-string HexToString(string Data);
-int HexToInt(char Convert);
+bool VerifySession();
+string ConnectToGameService();
+void DisconnectFromGameService();
 string EnterGame();
+void LockInbox();
+void UnlockInbox();
+void SendMessageToBox(std::string Message, int Box);
+bool CheckSlot(int Box, int Slot);
+string GetMessage(int Box, int Slot);
+void SetMessage(int Box, int Slot, string Message);
+string GetValueFromKey(string FindKey);
+void getFormData();
 
 int main(){
 	string Op;
@@ -75,7 +119,7 @@ int main(){
 				
 				UserFile.open ("u/" + u + "/u", fstream::in);
 				while ( UserFile >> Temp ){
-					if (i == 0) {Response += "&ID" + to_string(j) + "=" + Temp;}
+					if (i == 0) {Response += "&c" + to_string(j) + "=" + Temp;}
 					if (i == 1) {Response += "&Name" + to_string(j) + "=" + Temp;}
 					if (i == 2) {Response += "&Level" + to_string(j) + "=" + Temp;}
 					if (i == 3) {Response += "&Race" + to_string(j) + "=" + Temp;}
@@ -124,42 +168,12 @@ int main(){
 		exit(0);
 	}
 
-	//Character Login
-	if (Op == "Play"){
-// 		vector <string> Rebuild;
-// 		string ID = GetValueFromKey("ID");
-// 		
-// 		if (fileExists ("u/" + u)){
-// 			string PassTestIn(crypt(p.c_str(),"AB"));  //This is EXTREMELY insecure.  Use for student project only.
-// 			string PassTestFile;
-// 			
-// 			//Verify password for action
-// 			PWFile.open ("u/" + u + "/p", fstream::in);
-// 			PWFile >> PassTestFile;
-// 			PWFile.close();
-// 			
-// 			if (PassTestFile == PassTestIn){
-// 				Response += EnterGame();
-// 				cout << Response;
-// 				exit(0);
-// 			} else {
-// 				Response += "&Status=Fail&Reason=Incorrect Password.";
-// 				cout << Response;
-// 				exit(0);	
-// 			}
-// 		} else {
-// 			Response += "&Status=Fail&Reason=User does not exist.";
-// 			cout << Response;
-// 			exit(0);	
-// 		}	
-	}
-
 	//Character Setup
 	if (Op == "NewCharacter"){
 		vector <string> Rebuild;
 
 
-		string ID = GetValueFromKey("ID");
+		string charID = GetValueFromKey("c");
 		
 		if (fileExists ("u/" + u)){
 			string PassTestIn(crypt(p.c_str(),"AB"));  //This is EXTREMELY insecure.  Use for student project only.
@@ -191,7 +205,7 @@ int main(){
 				for (int i = 0; i < Rebuild.size(); i++){
 					UserFile << Rebuild[i] << endl;
 				}
-				UserFile << GetValueFromKey("ID") << endl;
+				UserFile << GetValueFromKey("c") << endl;
 				UserFile << GetValueFromKey("Name") << endl;
 				UserFile << GetValueFromKey("Level") << endl;
 				UserFile << GetValueFromKey("Race") << endl;
@@ -199,7 +213,7 @@ int main(){
 				UserFile.close();
 				
 				//Create the character information file for the character.
-				CharFile.open ("u/" + u + "/" + ID, fstream::out);
+				CharFile.open ("u/" + u + "/" + charID, fstream::out);
 				CharFile << "Name=" + GetValueFromKey("Name") << endl;
 				CharFile << "Race=" + GetValueFromKey("Race") << endl;
 				CharFile << "Class=" + GetValueFromKey("Class") << endl;
@@ -233,7 +247,7 @@ int main(){
 		vector <string> Rebuild;
 		string u = GetValueFromKey("u");
 		string p = GetValueFromKey("p");
-		string ID = GetValueFromKey("ID");
+		string charID = GetValueFromKey("c");
 			fstream PWFile;
 		
 		if (fileExists ("u/" + u)){
@@ -259,7 +273,7 @@ int main(){
 				//Write out list of characters removing the deleted character.
 				UserFile.open ("u/" + u + "/u", fstream::out | fstream::trunc);
 				for (int i = 0; i < Rebuild.size(); i++){
-					if (Rebuild[i] == ID){
+					if (Rebuild[i] == charID){
 						i += 4;
 					} else {
 						UserFile << Rebuild[i] << endl;
@@ -268,7 +282,7 @@ int main(){
 				UserFile.close();
 				
 				//Delete the character information file for the character.
-				string Obj = "u/" + u + "/" + ID;
+				string Obj = "u/" + u + "/" + charID;
 				remove(Obj.c_str());
 				cout << Response;
 				exit(0);
@@ -283,7 +297,85 @@ int main(){
 			exit(0);	
 		}	
 	}   
-    
+
+	//Character Login
+	if (Op == "EnterGame"){		
+		if (fileExists ("u/" + u)){
+			string PassTestIn(crypt(p.c_str(),"AB"));  //This is EXTREMELY insecure.  Use for student project only.
+			string PassTestFile;
+			
+			//Verify password for action
+			PWFile.open ("u/" + u + "/p", fstream::in);
+			PWFile >> PassTestFile;
+			PWFile.close();
+			
+			if (PassTestFile == PassTestIn){
+				string TryConnect = ConnectToGameService();
+				if (TryConnect == ""){
+					Response += EnterGame();
+				} else {
+					Response += TryConnect;
+				}
+				cout << Response;
+				DisconnectFromGameService();
+				exit(0);
+			} else {
+				Response += "&Status=Fail&Reason=Incorrect Password.";
+				cout << Response;
+				exit(0);	
+			}
+		} else {
+			Response += "&Status=Fail&Reason=User does not exist.";
+			cout << Response;
+			exit(0);	
+		}	
+	}
+	
+	//Make an in-game action (send message to service)
+	if (Op == "Play"){	
+		//Connect to game service
+		ConnectToGameService();
+		
+		//VerifySession
+		if (VerifySession()){
+			//Send Message
+			SendMessageToBox(SerialFormData,0);			
+		} else {
+			Response += "&Status=Fail&Reason=Session credentials do not match.";
+			cout << Response;
+		}
+		
+		//Close Session
+		DisconnectFromGameService();
+		exit(0);	
+	}
+	
+	//Update information from game service
+	if (Op == "Update"){	
+		int Box = stoi(GetValueFromKey("b"));
+		string RetVal;
+		
+		//Connect to game service
+		ConnectToGameService();
+		
+		//VerifySession
+		if (VerifySession()){
+			//Get Messages
+			for (int i = 1; i < slotCount; i++){
+				if (CheckSlot(Box, i)){
+					//Host interprets ^ as message dilineator.
+					RetVal += "^" + GetMessage(Box, i);
+				}
+			}			
+		} else {
+			Response += "&Status=Fail&Reason=Session credentials do not match.";
+			cout << Response;
+		}
+		
+		//Close Session
+		DisconnectFromGameService();
+		exit(0);	
+	}    
 }
 
 bool fileExists(const std::string& file) {
@@ -291,26 +383,17 @@ bool fileExists(const std::string& file) {
 	return (stat(file.c_str(), &buf) == 0);
 }
 
-string EnterGame(){
-	string RetVal;	
-	int tSlot = 0;
-	int Slot = 0;
-	struct timespec req, rem;
-	req.tv_sec = 0;
-	req.tv_nsec = 3000000L;
-	bool Done = false;
-		
-	//shared Memory ID and pointer for control segment.
-	int controlID;
-	char* controlPointer;
+bool VerifySession(){
+}
 
-	//Randomly generated key value for shared memory: 920004285
-	key_t controlKey = 920004285;
-	
-	//The shared memory contains 1024 segments of 8192 bytes (8KB) in length.
-	size_t controlSize = 8388608;
+string ConnectToGameService(){
+	string RetVal = "";	
+	int NewBox = 0;
+	char* MemLoc;
+	char Temp;
+	string BoxClaimMessage;
     	
-	//Attempt to create a new memory segment.
+	//Attempt to create a new connection to the shared memory segment.
 	controlID = shmget(controlKey, controlSize, S_IRUSR | S_IWUSR);
 	
 	if (controlID == -1) {
@@ -321,56 +404,118 @@ string EnterGame(){
 		
 	//Attach to the memory segment.
 	controlPointer = (char*)shmat(controlID, NULL, 0);
-
-	//Search for an empty comm slot.
-	//Note, I use a simple locking mechanism here that will greatly reduce R/W collisions, but is not actually
-	//completely thread-safe.  That's because between checking a zero bit and setting the bit to one, this process
-	//can be made to sleep or another process on another CPU can intervene.  It's much better than nothing and is
-	//simple enough for now.
 	
-	//Mailbox format:  Byte 0 = Taken/Free flag; Byte 1 = Message for server flag; Byte 2 = Message for client flag.  
-	//Byte 3 + 4 = size of message.  Byte 5+ = message.
-	//No thread issues since only one server-client per segment and always only read or write not both.
+	//Access semaphore	
+	semID = sem_open (semName.c_str(), 0);
 	
-	for (int i = 1; i < 255; i++){ 
-		
-		tSlot = i * 32768;
+	if (semID == SEM_FAILED) {
+		//Fatal error.  Unknown error with sem_open.
+		RetVal += "&Status=Fail&Reason=Unknown error with sem_open.";
+		return RetVal;
+	}
+}
 
-		//The first byte in a segment indicates ownership.  Byte = One if claimed and Zero if unclaimed.
-		if (controlPointer[tSlot] == 0x0){
-			//Claim the slot.
-			controlPointer[tSlot] = 0x1;
-			
-			string User = GetValueFromKey("u");
-			
-			//Message for Server Flag
-			controlPointer[tSlot+1] = 0x1;
-			//Size of Message = 1
-			controlPointer[tSlot+4] = (unsigned char)User.size();
-			//Message = ID of noew Box owner
-			sprintf(controlPointer[tSlot+5], User.c_str());
-		}
-		
+void DisconnectFromGameService(){
+	shmdt(controlPointer);
+}
+
+string EnterGame(){
+	string RetVal;	
+	int NewBox = 0;
+	char* MemLoc;
+	char Temp;
+	string BoxClaimMessage;
+    	
+	//Search for an empty communication box.
+	
+	for (int i = 1; i < boxCount; i++){
+		MemLoc = controlPointer + inboxSize + (i * boxSize);
+		Temp = MemLoc[BoxClaimedFlag];
+		MemLoc[BoxClaimedFlag] = 0x1;
+		if (Temp == 0x0){
+			NewBox = i;
+			break;
+		}		
 	}
 	
-	//Wait for a response from the game service.
-	//Response will incluse a session ID, confirmed slot number for message exchange, starting position, 
-	//full character stats, map, nearby enemies...
+	//No box found.  This means all boxes are in use.  Fail because too many players.
 	
-
-	
-	RetVal += "&Slot=" + to_string(Slot);
-	
-	while (controlPointer[Slot * 32768 + 2] == 0x0){
-		// Sleep for 3 microseconds and then check again.
-		nanosleep (&req, &rem);	
+	if (NewBox == 0){
+		RetVal += "&Status=Fail&Reason=Session full. Too many players.";
+		return RetVal;		
 	}
 	
-	size_t messageLen = (int)((unsigned char)controlPointer[3]) * 256 + (int)((unsigned char)controlPointer[4]);
+	//Box found.  Inform the game server that a box is reserved, and initiate a game session.
 	
-	string Temp(reinterpret_cast<const char *>(controlPointer[5]), messageLen);
-	RetVal += Temp;
+	BoxClaimMessage = "&u=" + GetValueFromKey("u") + "&c=" + GetValueFromKey("c") + "&s=" + GetValueFromKey("s") + "&Box=" + to_string(NewBox) + "&Action=EnterGame";
+	
+	LockInbox();
+	SendMessageToBox(BoxClaimMessage, 0);
+	UnlockInbox();
+
+	RetVal += "&Status=Success&Box=" + to_string(NewBox);
+
 	return RetVal;
+}
+
+void LockInbox(){
+	sem_wait(semID);
+}
+
+void UnlockInbox(){
+	sem_post(semID);
+}
+
+void SendMessageToBox(std::string Message, int Box){
+	//Note: a full box results in a dropped message.
+	for (int i = 1; i < slotCount; i++){
+		if (CheckSlot(Box, i)){               
+			SetMessage(Box, i, Message);
+			break;
+		}
+	}
+}
+
+bool CheckSlot(int Box, int Slot){	
+	// For future reference - this is not server-side thread-safe, modify if expanding use to other programs.
+	//To be thread safe, atomically reserve while checking (as we do with locking the Inbox.)
+	char* MemLoc;
+	bool RetVal;
+	
+	if (Box == 0 ){
+		MemLoc = controlPointer + (Slot * slotSize);
+		RetVal = MemLoc[MessageSetFlag] == 0x0;
+	} else {
+		MemLoc = controlPointer + inboxSize + (Box * boxSize) + (Slot * slotSize);
+		RetVal = (MemLoc[MessageSetFlag] == 0x1 && MemLoc[MessageReceivedFlag] == 0x0);
+	}
+	return (RetVal);
+}
+
+string GetMessage(int Box, int Slot){
+	char Buffer[slotSize];
+	char* MemLoc;
+	if (Box == 0 ){
+		MemLoc = controlPointer + (Slot * slotSize);
+	} else {
+		MemLoc = controlPointer + inboxSize + (Box * boxSize) + (Slot * slotSize);
+	}
+	memcpy (&Buffer, MemLoc + FlagSetSize, slotSize - FlagSetSize); 
+	return (string(Buffer)); 
+}
+
+void SetMessage(int Box, int Slot, string Message){ 
+	char Buffer[slotSize];
+	char* MemLoc;
+	if (Box == 0 ){
+		MemLoc = controlPointer + (Slot * slotSize);
+	} else {
+		MemLoc = controlPointer + inboxSize + (Box * boxSize) + (Slot * slotSize);
+	}
+	*(MemLoc + MessageSetFlag) = 0x1;
+	int MemSize = Message.size();
+	if (MemSize > slotSize - FlagSetSize) { MemSize = slotSize - FlagSetSize;}
+	memcpy ( MemLoc + FlagSetSize, Message.c_str(), MemSize); 
 }
 
 string GetValueFromKey(string FindKey){
@@ -396,6 +541,8 @@ void getFormData(){
 		EnvLen = TEMPstr.size();
 		Env = TEMPstr;
 	}
+	
+	SerialFormData = Env.substr(0,EnvLen);
 	
 	//cin can technically recieve more than CONTENT_LENGTH.
 	//We'll use EnvLen to restrict what we read.
