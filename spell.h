f482785115389f98b97592fc78827373f1a23874
Christopher Rodney <ctrodney@gmail.com>
2016-11-28 12:16:07 -0800

    fixing compile errors

--- spell.h
+++ spell.h
@@ -14,7 +14,7 @@ class Spell {
     std::string get_title();
     std::string get_description();
     int get_range();
-    void cast_spell(Entity *casting_char, Entity *target);
+    void cast_spell(Entity& casting_char, Entity& target);
   /*  todo: 
     have spells modified by character stats e.g. fireball damage affected by int
     work on spells that temporarily alter stats
