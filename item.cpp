e65c54c3c1411e2c87e66277469da06a437d9a27
Lennart Rudolph <lrudolph@hmc.edu>
2016-11-28 17:46:05 -0800

    removed durability from item class to reduce complexity

--- item.cpp
+++ item.cpp
@@ -2,89 +2,79 @@
 #include <string>
 #include "enums.h"
 #include "item.h"
-	
+    
 /************************************************************************************
 This is the base class for all items. It includes a description, affected stats,
 value, range, type, and rarity. Item drops and skills haven't been implemented yet. 
 *************************************************************************************/
 
-		// Setters/getters for affected stats. We should assume that the presence of an
-		// equipped item in a character's inventory simply boosts the character's stats.
-		// When combat damage is computed, the item will be modifying the character's base
-		// stats, so combat damage should simply take into account the final character stats 
-		// without knowing anything about the item stats.
+        // Setters/getters for affected stats. We should assume that the presence of an
+        // equipped item in a character's inventory simply boosts the character's stats.
+        // When combat damage is computed, the item will be modifying the character's base
+        // stats, so combat damage should simply take into account the final character stats 
+        // without knowing anything about the item stats.
 
         // Getter function for item name
         std::string Item::GetName() { 
             return name;
         }
 
-		// Getter function for description
-    	std::string Item::GetDescription() { 
-    		return description;
-    	}
+        // Getter function for description
+        std::string Item::GetDescription() { 
+            return description;
+        }
 
         // Getter function for item ID
         int Item::GetItemId() { 
             return itemId;
         }
-    	
-    	// Getter function for rarity
-    	std::string Item::GetRarity() { 
-    		return rarity;
-    	}
-    	
-    	// Getter function for itemType
-    	std::string Item::GetItemType() { 
-    		return itemType;
-    	}    
+        
+        // Getter function for rarity
+        std::string Item::GetRarity() { 
+            return rarity;
+        }
+        
+        // Getter function for itemType
+        std::string Item::GetItemType() { 
+            return itemType;
+        }    
 
-    	// retrieve the current or max value of the item stat in position "stat" of the ItemStats enum
-    	int Item::GetStat (Stats stat, bool Max) {
-        	return StatTable[Max ? Maximum : Current][(int) stat];
-    	}
+        // retrieve the current or max value of the item stat in position "stat" of the ItemStats enum
+        int Item::GetStat (Stats stat, bool Max) {
+            return StatTable[Max ? Maximum : Current][(int) stat];
+        }
 
-    	// alter the current or max value of the stat by amount
-    	void Item::AlterStat (Stats Stat, bool Max, int Amount){
-        	StatTable[Max ? Maximum : Current][(int) Stat] += Amount;
-    	}
+        // alter the current or max value of the stat by amount
+        void Item::AlterStat (Stats Stat, bool Max, int Amount){
+            StatTable[Max ? Maximum : Current][(int) Stat] += Amount;
+        }
     
-    	// save the current temporary state of a stat as a permanent state
-    	void Item::SetStatToMax (Stats Stat) {
-        	StatTable[Current][(int) Stat] = StatTable[Maximum][(int)Stat];
-    	}	
-							
-		// Getter for value
-		int Item::GetValue() {
-			return value;
-		}
+        // save the current temporary state of a stat as a permanent state
+        void Item::SetStatToMax (Stats Stat) {
+            StatTable[Current][(int) Stat] = StatTable[Maximum][(int)Stat];
+        }   
+                            
+        // Getter for value
+        int Item::GetValue() {
+            return value;
+        }
 
-		// Setter for value
-		void Item::SetValue(int newValue) {
-			value = newValue;
-		}
+        // Setter for value
+        void Item::SetValue(int newValue) {
+            value = newValue;
+        }
 
-		// Getter for max_range
-		int Item::GetMaxRange() {
-			return max_range;
-		}
+        // Getter for max_range
+        int Item::GetMaxRange() {
+            return max_range;
+        }
 
-		// Setter for max_range
-		void Item::SetMaxRange(int newRange) {
-			max_range = newRange;
-		}
+        // Setter for max_range
+        void Item::SetMaxRange(int newRange) {
+            max_range = newRange;
+        }
 
-		// Getter for condition
-		bool Item::GetCondition() { 
-			return isBroken;
-		}
-
-		// Setter for condition
-		void Item::SetCondition(bool condition) { 
-			isBroken = condition;
-		}
-
-		// Stores value, max_range, length of rarity, rarity, length of description,
+        // Stores value, max_range, length of rarity, rarity, length of description,
         // description, length of type, type, and isBroken 
         void Item::WriteToStream(std::ostream & str) {
             // Write length and data for name
@@ -111,17 +101,13 @@ value, range, type, and rarity. Item drops and skills haven't been implemented y
             str.write(reinterpret_cast<char *>(&description_length),sizeof(int));
             str.write(description.data(), description_length);
             
-        	// Write length and data for itemType
+            // Write length and data for itemType
             int itemType_length = itemType.length();
             str.write(reinterpret_cast<char *>(&itemType_length),sizeof(int));
             str.write(itemType.data(), itemType_length);
-            
-            // Write isBroken (1 for true 0 for false)
-            str.write(reinterpret_cast<char *>(&isBroken),sizeof(bool));
-            // todo: convert the bool to a number
         }
 
- 		// Reads the data in the format written by Item::pack
+        // Reads the data in the format written by Item::pack
         void Item::ReadFromFile(std::istream & str) {
             const int BUFFER_SIZE = 256;
             static char buffer[256];
@@ -136,7 +122,7 @@ value, range, type, and rarity. Item drops and skills haven't been implemented y
             // Get item ID
             str.read(reinterpret_cast<char *>(&itemId),sizeof(itemId));           
             
-        	// Get value
+            // Get value
             str.read(reinterpret_cast<char *>(&value),sizeof(value));
             
             // Get max_range
@@ -162,18 +148,15 @@ value, range, type, and rarity. Item drops and skills haven't been implemented y
             str.read(buffer, itemType_length);
             buffer[itemType_length] = '\0';
             itemType = buffer;
-            
-            // Get the isBroken bool
-            // todo
         }
         
         // This function converts a template data type to a string and adds 
         // a '/' to end of string as a delimiter.
-    	template<typename T> std::string Item::ConvertToString(T data){
-    		std::stringstream stream;
-    		std::string str;
-    		stream << data;
-    		stream >> str;
-    		str += '/';
-    		return str;
-	}
+        template<typename T> std::string Item::ConvertToString(T data){
+            std::stringstream stream;
+            std::string str;
+            stream << data;
+            stream >> str;
+            str += '/';
+            return str;
+    }
--- item.h
+++ item.h
@@ -1,55 +1,34 @@
 class Item {
     public:
-	
     Item();
     ~Item();
-
     std::string GetName();
-
     int GetItemId(); 
-
     std::string GetDescription();  
-
     std::string GetRarity();  
-
     std::string GetItemType();  
-
     int GetStat (Stats stat, bool Max); 
-
     void AlterStat (Stats Stat, bool Max, int Amount);
-
     void SetStatToMax (Stats Stat); 
-
     int GetValue(); 
-
     void SetValue(int newValue); 
-
     int GetMaxRange(); 
-
     void SetMaxRange(int newRange); 
-
-    bool GetCondition();  
-
-    void SetCondition(bool condition);  
-
     void WriteToStream(std::ostream & str);          
-
     void ReadFromFile(std::istream & str); 
-
     template<typename T> std::string ConvertToString(T data);
 
-	private:
+    private:
     std::string name;
     int itemId;
-	int value;
-	int max_range;
+    int value;
+    int max_range;
     std::string description;
     std::string rarity;
     std::string itemType;   
-	// This StatTable holds a row  for Current item stats and a row for Maximum item stats.
-	// See enums.h for the ItemStats enum. 
-	int StatTable[2][6]; 
-	bool isBroken; // true indicates that the item is broken
-	const int Current = 0; // temporarily affects the stat
-	const int Maximum = 1; // permanently affects the stat
+    // This StatTable holds a row  for Current item stats and a row for Maximum item stats.
+    // See enums.h for the ItemStats enum. 
+    int StatTable[2][6]; 
+    const int Current = 0; // temporarily affects the stat
+    const int Maximum = 1; // permanently affects the stat
 };
