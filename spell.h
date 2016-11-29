f8bdb402f4fc060e96c227ec1444df356875943d
dhoang1csuf <dhoang1@csu.fullerton.edu>
2016-11-28 17:21:13 -0800

    Update spell.h

--- spell.h
+++ spell.h
@@ -15,6 +15,14 @@ class Spell {
     std::string get_description();
     std::int get_range();
     void cast_spell(Entity *casting_char, Entity *target);
+  /*  todo: 
+    have spells modified by character stats e.g. fireball damage affected by int
+    work on spells that temporarily alter stats
+    
+    hp changing spells: fireball, heal, frostbolt, powershot, fortify(temp), resurrect, thunderbolt
+    stat altering: frostbolt, haste, haze, slow, radiance, fortify, focus, shield
+    special: resurrect
+  */
     
    private:
     
