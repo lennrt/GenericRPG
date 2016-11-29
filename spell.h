e9e3886281339d30f52607032aa0decb645e5c15
dhoang1csuf <dhoang1@csu.fullerton.edu>
2016-11-28 16:22:52 -0800

    Update spell.h

--- spell.h
+++ spell.h
@@ -13,18 +13,16 @@ class Spell {
     ~Spell();
     std::string get_title();
     std::string get_description();
-    int get_value();
-    int get_mana_cost();
-    int get_range();
+    std::int get_range();
     void cast_spell(Entity *casting_char, Entity *target);
     
    private:
     
     std::string title;
     std::string description;
-    int value;
-    int mana_cost;
-    int range;
-    Stats target_stat;
+    //int value;
+    //int mana_cost;
+    std::int range;
+    //std::Stats target_stat;
     
 };
