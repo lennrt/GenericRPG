41c00923500cd03eae3bfc0b9f3e55d0056cefa5
Nhat Nguyen <rockylikespocky@Nhats-MacBook-Air.local>
2016-11-28 17:01:55 -0800

    Added map.h and map.cpp with 2D array called mapArray

---
+++ map.cpp
@@ -0,0 +1,15 @@
+//
+//  map.cpp
+//  map
+//
+//  Created by Nhat Nguyen on 11/27/16.
+//  Copyright © 2016 Nhat Nguyen. All rights reserved.
+//
+
+#include "tile.h"
+
+/* Map Class Constructors */
+
+Map::Map() {
+    // Empty Constructor
+}
---
+++ map.h
@@ -0,0 +1,30 @@
+//
+//  map.h
+//  map
+//
+//  Created by Nhat Nguyen on 11/27/16.
+//  Copyright © 2016 Nhat Nguyen. All rights reserved.
+//
+
+#ifndef map_h
+#define map_h
+
+#include <stdio.h>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "enums.h"
+#include "tile.h"
+
+using namespace std;
+
+class Map {
+private:
+    int mapArray [100][100];
+    
+public:
+    Map();
+};
+
+#endif /* map_h */
