8a063bacdf0215fd3acb61f80f7e4b62922495db
Christopher Rodney <ctrodney@gmail.com>
2016-11-30 15:13:18 -0800

    Work in progress

--- accman.cpp
+++ accman.cpp
@@ -354,22 +354,20 @@ int main(){
 		int Box = stoi(GetValueFromKey("b"));
 		string RetVal;
 		
-		RetVal += "StartUpdate";
-		
 		//Connect to game service
 		ConnectToGameService();
-		RetVal += "Connected";
+		
 		//VerifySession
 		if (VerifySession()){
-			RetVal += "Verified";
 			//Get Messages
 			for (int i = 1; i < slotCount; i++){
 				if (CheckSlot(Box, i)){
 					//Host interprets ^ as message dilineator.
 					RetVal += "^" + GetMessage(Box, i);
 				}
-			}		
-			RetVal += "Box" + to_string(Box) + " was checked.";
+			}	
+			Response += RetVal;
+			cout << Response;
 		} else {
 			Response += "&Status=Fail&Reason=Session credentials do not match.";
 			cout << Response;
