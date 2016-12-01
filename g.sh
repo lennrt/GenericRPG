eeb720e48484e21c16387b78cd2b2b1df69f2468
Christopher Rodney <ctrodney@gmail.com>
2016-12-01 01:54:05 -0800

    Shell script

--- g.sh
+++ g.sh
@@ -2,12 +2,14 @@ g++ -std=c++0x entity.cpp -c -o entity.o
 g++ -std=c++0x monster.cpp -c -o monster.o
 g++ -std=c++0x character.cpp -c -o character.o
 g++ -std=c++0x tile.cpp -c -o tile.o
+g++ -std=c++0x map.cpp -c -o map.o
 g++ -std=c++0x spell.cpp -c -o spell.o
 g++ -std=c++0x item.cpp -c -o item.o
 g++ -std=c++0x mailbox.cpp -c -o mailbox.o
 g++ -std=c++0x timetable.cpp -c -o timetable.o
-g++ -std=c++0x driver.cpp -o driver.cgi mailbox.o timetable.o entity.o monster.o character.o tile.o spell.o item.o -lcrypt -lrt -lpthread
+g++ -std=c++0x driver.cpp -o driver mailbox.o timetable.o entity.o monster.o character.o tile.o spell.o item.o map.o -lcrypt -lrt -lpthread
+chmod 0755 driver
 
 g++ -std=c++0x accman.cpp -o accman.cgi -lcrypt -lrt -lpthread
-sudo chmod 4755 accman.cgi
- 
+chmod +s accman.cgi
+rm *.o
