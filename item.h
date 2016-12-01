f71d515952e40da2895506daa531b62b666cead8
ctrodney <ctrodney@gmail.com>
2016-11-30 16:07:53 -0800

    Update item.h

--- item.h
+++ item.h
@@ -1,55 +1,41 @@
+#include <vector>
+
+struct StatEffect {
+   Stats Stat;
+   int Modifier;
+   bool Permanent;
+};
+
 class Item {
     public:
-	
     Item();
     ~Item();
-
     std::string GetName();
+    void SetName(std::string newName);
+    int GetItemId();
+    void SetItemId(int newId);
+    std::string GetDescription();
+    std::string GetRarity();
+    std::string GetItemType();
+    void SetDescription(std::string newDesc);
+    void SetRarity(std::string newRarity);
+    void SetItemType(std::string newItemType);
+    int GetValue();
+    void SetValue(int newValue);
+    int GetMaxRange();
+    void SetMaxRange(int newRange);
+    StatEffect GetStat(int degree);
+    void SetStat(int degree, Stats Stat, int modifier, bool permanent);
+    std::vector<StatEffect> GetStats();
+    void SetStats(std::vector<StatEffect> effectsVector);
 
-    int GetItemId(); 
-
-    std::string GetDescription();  
-
-    std::string GetRarity();  
-
-    std::string GetItemType();  
-
-    int GetStat (Stats stat, bool Max); 
-
-    void AlterStat (Stats Stat, bool Max, int Amount);
-
-    void SetStatToMax (Stats Stat); 
-
-    int GetValue(); 
-
-    void SetValue(int newValue); 
-
-    int GetMaxRange(); 
-
-    void SetMaxRange(int newRange); 
-
-    bool GetCondition();  
-
-    void SetCondition(bool condition);  
-
-    void WriteToStream(std::ostream & str);          
-
-    void ReadFromFile(std::istream & str); 
-
-    template<typename T> std::string ConvertToString(T data);
-
-	private:
+    private:
     std::string name;
     int itemId;
-	int value;
-	int max_range;
+    int value;
+    int max_range;
     std::string description;
     std::string rarity;
-    std::string itemType;   
-	// This StatTable holds a row  for Current item stats and a row for Maximum item stats.
-	// See enums.h for the ItemStats enum. 
-	int StatTable[2][6]; 
-	bool isBroken; // true indicates that the item is broken
-	const int Current = 0; // temporarily affects the stat
-	const int Maximum = 1; // permanently affects the stat
+    std::string itemType;
+    std::vector<StatEffect> statEffects;
 };
