4bc3e862471bd431da453768d79fa8e95e7fab71
Christopher Rodney <ctrodney@gmail.com>
2016-12-02 20:21:30 -0800

    Work in progress

--- item.h
+++ item.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 
 struct StatEffect {
    Stats Stat; 
@@ -39,3 +40,4 @@ class Item {
     std::string itemType;   
     std::vector<StatEffect> statEffects;
 };
+
